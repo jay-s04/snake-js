93d82b8387a16295df314be5fa30ca3cbb023d09
Jay <80469071+jay-s04@users.noreply.github.com>
2024-02-25 21:45:14 -0500

    Implement basic logic to draw snake, generate food, update board, etc.

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const board = document.getElementById("game-board");
+const instructionText = document.getElementById("instruction-text");
+const logo = document.getElementById("logo");
 
+const gridSize = 20;
 let snake = [{x: 10, y: 10}];
+let food = generateFood();
+let direction = 'right';
+let gameInterval;
+let gameSpeedDelay = 200;
+let gameStarted = false;
+
 function draw() {
     board.innerHTML = '';
     drawSnake();
+    drawFood();
 }
 
 function drawSnake() {
@@ -27,4 +37,56 @@ function setPosition(element, position) {
 
 }
 
-draw();
+function drawFood() {
+    const foodElement = createGameElement('div', 'food');
+    setPosition(foodElement, food);
+    board.appendChild(foodElement);
+}
+
+function generateFood() {
+    const x = Math.floor(Math.random() * gridSize) + 1;
+    const y = Math.floor(Math.random() * gridSize) + 1;
+    return {x, y};
+}   
+
+function move() {
+    const head = { ...snake[0] };
+    switch (direction) {
+        case 'right':
+            head.x++;
+            break;
+        case 'left':
+            head.x--;
+            break;
+        case 'up':
+            head.y--;
+            break;
+        case 'down':
+            head.y++;
+            break;
+    }
+    snake.unshift(head);
+
+    if (head.x === food.x && head.y === food.y) {
+        food = generateFood()
+        clearInterval();
+        gameInterval = setInterval(() => {
+            move();
+            draw();
+        }, gameSpeedDelay)
+    } 
+    else {
+        snake.pop();
+    }
+}
+
+// setInterval(() => {
+//     move();
+//     draw();
+// }, 200);
+
+
+function startGame() {
+    gameStarted = true;
+
+}
